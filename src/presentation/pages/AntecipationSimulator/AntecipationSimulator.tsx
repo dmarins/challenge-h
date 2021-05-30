5a7458136355983bf6634ebb32d9ea2c44b85b6b
dmarins <diogodsfmarins@gmail.com>
2021-05-30 19:37:38 -0300

    funcionando a requisição

--- src/presentation/pages/AntecipationSimulator/AntecipationSimulator.tsx
+++ src/presentation/pages/AntecipationSimulator/AntecipationSimulator.tsx
@@ -1,5 +1,7 @@
 import IDefaultAntecipation from 'domain/usecases/default-antecipation/defaultAntecipation';
 
+import { useCallback, useEffect, useState } from 'react';
+
 import './AntecipationSimulator.css';
 
 type Props = {
@@ -7,6 +9,45 @@ type Props = {
 };
 
 const AntecipationSimulator = ({ defaultAntecipation }: Props): JSX.Element => {
+  const [amount, setAmount] = useState(null);
+  const [installments, setInstallments] = useState(null);
+  const [mdr, setMdr] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const trimValue = (value) => {
+    return (value = value ? value.trim() : null);
+  };
+
+  const convertToInt = (value) => {
+    return parseInt(value);
+  };
+
+  const handleAmountChange = (e) => {
+    const value = trimValue(e.target.value);
+    setAmount(convertToInt(value));
+  };
+  const handleInstallmentsChange = (e) => {
+    const value = trimValue(e.target.value);
+    setInstallments(convertToInt(value));
+  };
+  const handleMdrChange = (e) => {
+    const value = trimValue(e.target.value);
+    setMdr(convertToInt(value));
+  };
+
+  const initPost = useCallback(async () => {
+    const result = await defaultAntecipation.post(amount, installments, mdr);
+    setResult(result);
+  }, [amount, defaultAntecipation, installments, mdr]);
+
+  useEffect(() => {
+    if (amount === null) return;
+    if (installments === null) return;
+    if (mdr === null) return;
+
+    initPost();
+  }, [amount, initPost, installments, mdr]);
+
   return (
     <section className="container">
       <div className="inputs">
@@ -14,16 +55,20 @@ const AntecipationSimulator = ({ defaultAntecipation }: Props): JSX.Element => {
         <div className="form">
           <div className="field">
             <label>Informe o valor da venda *</label>
-            <input type="text" />
+            <input type="text" name="amount" onChange={handleAmountChange} />
           </div>
           <div className="field">
             <label>Em quantas parcelas? *</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="installments"
+              onChange={handleInstallmentsChange}
+            />
             <small>Máximo de 12 parcelas</small>
           </div>
           <div className="field">
             <label>Informe o percentual de MDR *</label>
-            <input type="text" />
+            <input type="text" name="mdr" onChange={handleMdrChange} />
           </div>
         </div>
       </div>
