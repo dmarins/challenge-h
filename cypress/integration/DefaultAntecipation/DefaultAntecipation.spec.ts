4be2321762f37650c5b4b5d021453529c6824872
dmarins <diogodsfmarins@gmail.com>
2021-06-03 17:13:52 -0300

    testes

--- cypress/integration/DefaultAntecipation/DefaultAntecipation.spec.ts
+++ cypress/integration/DefaultAntecipation/DefaultAntecipation.spec.ts
@@ -17,4 +17,109 @@ describe('DefaultAntecipation', () => {
     cy.get('[data-test-id="value-30"]').contains('990,00');
     cy.get('[data-test-id="value-90"]').contains('990,00');
   });
+
+  it('deve estimular a validação de obrigatoridade do campo amount', () => {
+    cy.get('[data-test-id="amount"]').then((elem) => {
+      let title = elem.attr('title');
+      cy.wrap(title).should('eq', 'Campo obrigatório');
+    });
+
+    cy.get('[data-test-id="amount"]')
+      .type('1000')
+      .clear()
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Campo obrigatório');
+      });
+  });
+
+  it('deve estimular a validação de formato do campo amount', () => {
+    cy.get('[data-test-id="amount"]')
+      .type('aaa')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Apenas números são aceitos');
+      });
+  });
+
+  it('deve estimular a validação de limite do campo amount', () => {
+    cy.get('[data-test-id="amount"]')
+      .type('100')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'O valor não pode ser menor que 1000');
+      });
+  });
+
+  it('deve estimular a validação de obrigatoridade do campo installments', () => {
+    cy.get('[data-test-id="installments"]').then((elem) => {
+      let title = elem.attr('title');
+      cy.wrap(title).should('eq', 'Campo obrigatório');
+    });
+
+    cy.get('[data-test-id="installments"]')
+      .type('1')
+      .clear()
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Campo obrigatório');
+      });
+  });
+
+  it('deve estimular a validação de formato do campo installments', () => {
+    cy.get('[data-test-id="installments"]')
+      .type('aaa')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Apenas números são aceitos');
+      });
+  });
+
+  it('deve estimular a validação de limite do campo installments', () => {
+    cy.get('[data-test-id="installments"]')
+      .type('15')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'O valor deve estar entre 1 e 12');
+      });
+  });
+
+  it('deve estimular a validação de obrigatoridade do campo mdr', () => {
+    cy.get('[data-test-id="mdr"]').then((elem) => {
+      let title = elem.attr('title');
+      cy.wrap(title).should('eq', 'Campo obrigatório');
+    });
+
+    cy.get('[data-test-id="mdr"]')
+      .type('1')
+      .clear()
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Campo obrigatório');
+      });
+  });
+
+  it('deve estimular a validação de formato do campo mdr', () => {
+    cy.get('[data-test-id="mdr"]')
+      .type('aaa')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'Apenas números são aceitos');
+      });
+  });
+
+  it('deve estimular a validação de limite do campo mdr', () => {
+    cy.get('[data-test-id="mdr"]')
+      .type('0')
+      .wait(300)
+      .then((elem) => {
+        let title = elem.attr('title');
+        cy.wrap(title).should('eq', 'O valor deve estar entre 1 e 100');
+      });
+  });
 });
